ca6dc868d9dd57ace6d3b347ffa3b1372217a4e5
Brian Salemi <briandsalemi@gmail.com>
2019-04-27 13:18:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,8 @@ def my_each(array)
   if block_given?
     i = 0
     while i > array.length
-      yield do |i|
-        puts i
+      yield array[i]
         i += 1
-      end
     end
   end
 end
